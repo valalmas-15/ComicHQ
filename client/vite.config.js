ac25c3568beef0c2fd4d3f1e7f1ddc98448578be
Muhammad Nauval Almas <nauvalalmas@gmail.com>
2026-04-01 18:10:02 +0700

    asdf

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     allowedHosts: "all",
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://server:3000",
         changeOrigin: true,
       },
     },
