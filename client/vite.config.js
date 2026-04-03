7f35e7d19c9dc14a9120bf5b8dff8e35032bb287
Muhammad Nauval Almas <nauvalalmas@gmail.com>
2026-04-03 11:37:28 +0700

    update fullscreen ios

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: "0.0.0.0",
     https: false,
     port: 3001,
-    allowedHosts: "all",
+    allowedHosts: true,
     hmr: {
       // Comment these out for local development access (192.168.1.50:3001)
       // Enable them only if you are using a proxy (like Cloudflare Tunnel) with HTTPS
